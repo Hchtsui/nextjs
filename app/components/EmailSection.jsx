fed5b007efb7109a5b3cf2915654c471bca16eed
Hchtsui <112637278+Hchtsui@users.noreply.github.com>
2024-03-12 13:22:42 +0100

    added new thing

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -56,10 +56,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/Hchtsui">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/henry-tsui-6b0949203/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -6,56 +6,90 @@ import ProjectTag from "./ProjectTag";
 const projectsData = [
   {
     id: 1,
-    title: "React Portfolio Website",
-    description: "Project 1 description",
-    image: "/images/projects/1.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    title: "higher lower game",
+    description: "Project 1 higher lower game",
+    image: "/images/projects/image1.png",
+    tag: ["Game", "All"],
+    gitUrl: "https://github.com/Hchtsui/TeamKO.git",
+    previewUrl: "https://hchtsui.github.io/Higher-lower/",
   },
   {
     id: 2,
-    title: "Potography Portfolio Website",
-    description: "Project 2 description",
-    image: "/images/projects/2.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    title: "escape room game",
+    description: "project 2 escape room game",
+    image: "/images/projects/image2.png",
+    tag: ["All", "Game"],
+    gitUrl: "https://github.com/Hchtsui/escape-room.git",
+    previewUrl: "https://hchtsui.github.io/escape-room/",
   },
   {
     id: 3,
-    title: "E-commerce Application",
-    description: "Project 3 description",
-    image: "/images/projects/3.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    title: "dashboard",
+    description: "Project 3 making a dashboard about currency exchange rates",
+    image: "/images/projects/image3.png",
+    tag: ["All", "Web", "Game"],
+    gitUrl: "https://github.com/Hchtsui/dashboard-p04-henry.git",
+    previewUrl: "https://hchtsui.github.io/dashboard-p04-henry/valuta.html",
   },
   {
     id: 4,
-    title: "Food Ordering Application",
-    description: "Project 4 description",
-    image: "/images/projects/4.png",
-    tag: ["All", "Mobile"],
-    gitUrl: "/",
-    previewUrl: "/",
+    title: "portfolio website",
+    description:
+      "Project 4 this project i chose to make a portfolio website for myself",
+    image: "/images/projects/image4.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Hchtsui/Hchtsui.github.io.git",
+    previewUrl: "https://hchtsui.github.io/Themaweek2-Henry-profiel/",
   },
   {
     id: 5,
-    title: "React Firebase Template",
-    description: "Authentication and CRUD operations",
-    image: "/images/projects/5.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    title: "nought and crosses game",
+    description: "Project 5 nought and crosses game",
+    image: "/images/projects/image5.png",
+    tag: ["All", "Game"],
+    gitUrl: "https://github.com/Hchtsui/Boter-kaas-eieren.git",
+    previewUrl: "https://hchtsui.github.io/Boter-kaas-eieren/",
   },
   {
     id: 6,
-    title: "Full-stack Roadmap",
-    description: "Project 5 description",
-    image: "/images/projects/6.png",
+    title: "webcatalogue",
+    description: "Project 6 webcatalogue for a school project",
+    image: "/images/projects/image6.png",
     tag: ["All", "Web"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/Hchtsui/webcatalogus-Henry.git",
+    previewUrl:
+      "https://hchtsui.github.io/webcatalogus-Henry/subject-one/category-three.html",
+  },
+  {
+    id: 7,
+    title: "Shushi website",
+    description:
+      "Project 7 website for a sushi restaurant this code u cant see how it looks but only the code",
+    image: "/images/projects/image7.png",
+    tag: ["All", "Web"],
+    gitUrl:
+      "https://github.com/ROCMondriaanTIN/sd22-p5-projectzuzu-Hchtsui.git",
+    previewUrl: "/",
+  },
+  {
+    id: 8,
+    title: "review your experience",
+    description:
+      "Project 8 website for a review your experience for a school project",
+    image: "/images/projects/image8.jpg",
+    tag: ["All", "Web"],
+    gitUrl:
+      "https://github.com/ROCMondriaanTIN/sd22-p6-project-reviewyourexperience-henry.git",
+    previewUrl: "/",
+  },
+  {
+    id: 9,
+    title: "webshop",
+    description:
+      "Project 9 website selling products for clothing for a school project (currently working on it)",
+    image: "/images/projects/image9.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Hchtsui/p7_Creative_Creativity.git",
     previewUrl: "/",
   },
 ];
@@ -90,8 +124,8 @@ const ProjectsSection = () => {
           />{" "}
           <ProjectTag
             onClick={handleTagChange}
-            name="Mobile"
-            isSelected={Tag === "Mobile"}
+            name="Game"
+            isSelected={Tag === "Game"}
           />{" "}
         </div>
         <div className="grid md:grid-cols-3 gap-8 md:gap-12">
